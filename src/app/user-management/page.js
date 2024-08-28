2314aa12d19161501f5fc14383a747cbece51b18
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-08-28 21:04:04 +0530

    user commit

--- src/app/user-management/page.js
+++ src/app/user-management/page.js
@@ -1,7 +1,10 @@
 function UserMgmt() {
   return (
-    <div>
-      <h1>User Management</h1>
+    <div className="p-20 max-w-6xl">
+      <div classNmae="flex justify-between">
+        <h1>User Management</h1>
+        <addNewUser />
+      </div>
     </div>
   );
 }
---
+++ src/components/add-new-user/index.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { Button } from "../ui/button";
+
+function addNewUser() {
+  return (
+    <div>
+      <Button>Add new user</Button>
+    </div>
+  );
+}
+
+export default addNewUser;
